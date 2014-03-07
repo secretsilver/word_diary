599ce8e4f0c52e9a255cbd07d7bf64c5d846e2b0
Andy Huynh <andy4thehuynh@gmail.com>
2014-03-06 16:27:52 -0800

    Twitter Authentication wired.
    Index page isn't styled. Will add in future iterations

--- lib/worddiary.rb
+++ lib/worddiary.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'omniauth-twitter'
 require_relative 'worddiary/dictionary'
 require 'dotenv'
 Dotenv.load
@@ -8,16 +9,45 @@ Wordnik.configure do |config|
   config.api_key = ENV['WORDNIK_API_KEY'] 
 end
 
+
 class WordDiary < Sinatra::Base
+  enable :sessions
+
+  use OmniAuth::Builder do
+    provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
+  end
+
+  helpers do
+    def user?
+      session[:user]
+    end
+  end
 
 	get '/' do
-		erb :home
+    erb :index, :layout => false
 	end
+
+  get '/login' do
+    redirect to("/auth/twitter")
+  end
+
+  get '/logout' do
+    session[:admin] = nil
+    erb :index, :layout => false
+  end
+
+  get '/auth/twitter/callback' do
+    env['omniauth.auth'] ? session[:user] = true : halt(401, "Not Authorized")
+    erb :home
+  end
+
+  get '/auth/failure' do
+    params[:message]
+  end
 
 	get '/define' do
 		@word = params[:word]
 		dictionary = Dictionary.new
-
 		@definitions = dictionary.define_word(@word)
 		erb :define
 	end
